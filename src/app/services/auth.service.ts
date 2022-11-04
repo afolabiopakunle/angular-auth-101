32b11961e8f8a462dec99b687b8dfc24c4ca5e9b
Afolabi <opakunleafolabi@gmail.com>
2022-11-04 23:55:18 +0100

    auth service implemente

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(
+    username: string,
+    password: string
+  ): Observable<any> {
+    return this.http.post(AUTH_API + 'sign-in', {
+      username,
+      password
+    }, httpOptions)
+  }
+
+  register(
+    username: string,
+    email: string,
+    password: string
+  ):Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      username,
+      email,
+      password
+    }, httpOptions)
+  }
+
 }
